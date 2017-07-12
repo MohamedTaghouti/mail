8b919d5f89ee6f3c1efafab3c187d044b1e57f46
Mohamed Taghouti <mohamed.taghouti@sofrecom.com>
2017-07-12 20:54:28 +0200

    - add body component
    - add dropdown in header
    
    missed : list of imgs in the body (next Step )

--- front-end/front-end-app/src/app/app.module.ts
+++ front-end/front-end-app/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import  { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
